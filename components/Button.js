b34bc461d931517ac058f9ab8f4030104022647e
Dheeraj Verma <dheerajverma55569@gmail.com>
2024-02-28 11:00:17 +0530

    Initial Commit

---
+++ components/Button.js
@@ -0,0 +1,37 @@
+/* eslint-disable prettier/prettier */
+// eslint-disable-next-line prettier/prettier
+import React from 'react';
+import {TouchableOpacity, Image, StyleSheet, Text} from 'react-native';
+
+import colors from '../constants/colors';
+
+const styles = StyleSheet.create({
+  button: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginVertical: 20,
+  },
+  buttonIcon: {
+    marginRight: 10,
+    width: 20,
+    height: 20,
+  },
+  buttonText: {
+    fontSize: 16,
+    color: colors.white,
+  },
+});
+
+export const Button = ({onPress, text}) => {
+  return (
+    <TouchableOpacity onPress={onPress} style={styles.button}>
+      <Image
+        source={require('../assets/images/reverse.png')}
+        style={styles.buttonIcon}
+        resizeMode="contain"
+      />
+      <Text style={styles.buttonText}>{text}</Text>
+    </TouchableOpacity>
+  );
+};
---
+++ constants/colors.js
@@ -0,0 +1,9 @@
+/* eslint-disable prettier/prettier */
+export default {
+  white: '#fff',
+  border: '#E2E2E2',
+  text: '#343434',
+  blue: '#4F6D7A',
+  textLight: '#797979',
+  offWhite: '#F0F0F0',
+};
